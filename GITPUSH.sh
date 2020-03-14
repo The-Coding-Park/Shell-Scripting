46d975c1eb77ce62c495081fa445a4a68c71cc41
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-03-14 23:07:31 +0530

    new code

--- GITPUSH.sh
+++ GITPUSH.sh
@@ -2,6 +2,6 @@ git add -A
 git commit -m "new code"
 git push --force-with-lease origin master
 clear
-echo "CHNAGES COMMITED AND PUSHED SUCCESSFULLY" | boxes
+echo "CHNAGES COMMITED AND PUSHED SUCCESSFULLY" | boxes -d boy
 sleep 5
 clear
