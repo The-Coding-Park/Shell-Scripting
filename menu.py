9e41792ecd6ab5dee8a066345f113bdf488c467f
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-03-19 12:19:00 +0530

    new update

--- menu.py
+++ menu.py
@@ -3,23 +3,23 @@ import subprocess
 import sys
 
 
+os.system("tput setaf 14")
+print("\t\t\t   Hello: Linux World")
+print("\t\t\t.........................")
 
-os.system("sudo su")
+os.system("tput setaf 3")
+print("""
+1. print Calender
+2. Show date
+3. Configure new web server
+4. Make Apine Docker container
+5. Destroy one pc connected with LAN
+        """)
 
-
-
-# if(os.system("echo $?>")==0):
-#     os.system("tput setaf 14")
-#     print("\t\t\t   Hello: Linux World")
-#     print("\t\t\t.........................")
-
-#     os.system("tput setaf 3")
-#     print("""
-#             1. print Calender
-#             2. Show date
-#             3. Configure new web server
-#             4. Make Apine Docker container
-#             5. Destroy one pc connected with LAN
-#             """)
-
-#     d=int(input("Enter your choice: "))
+d=int(input("Enter your choice: "))
+if (d==1):
+    os.system("cal")
+elif(d==2):
+    os.system("date")
+else:
+    print("Coming Soon !")
