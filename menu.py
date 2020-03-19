561348f88330a389c645fd5812d142fd0c91e731
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-03-19 12:12:56 +0530

    new code up

---
+++ menu.py
@@ -0,0 +1,25 @@
+import os 
+import subprocess
+import sys
+
+
+
+os.system("sudo su")
+
+
+
+# if(os.system("echo $?>")==0):
+#     os.system("tput setaf 14")
+#     print("\t\t\t   Hello: Linux World")
+#     print("\t\t\t.........................")
+
+#     os.system("tput setaf 3")
+#     print("""
+#             1. print Calender
+#             2. Show date
+#             3. Configure new web server
+#             4. Make Apine Docker container
+#             5. Destroy one pc connected with LAN
+#             """)
+
+#     d=int(input("Enter your choice: "))
